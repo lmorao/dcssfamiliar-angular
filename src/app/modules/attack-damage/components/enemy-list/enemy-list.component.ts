390b6c0b3a7edf2a833cf0906193016408b1ede9
Luis Morao <luis.n.morao@gmail.com>
2019-12-24 07:47:42 -0800

    Enemy list and images, google analytics, ac reduction

--- src/app/modules/attack-damage/components/enemy-list/enemy-list.component.ts
+++ src/app/modules/attack-damage/components/enemy-list/enemy-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Enemy } from '../../../../shared/models/enemy.model'
 import { EnemyListService} from '../../../../core/services/enemy-list.service'
+import { monsters } from '../../../../monsters'
 
 @Component({
   selector: 'app-enemy-list',
@@ -13,6 +14,13 @@ export class EnemyListComponent implements OnInit {
 
   lessac = function () {if (this.selectedEnemy.ac >1) {this.selectedEnemy.ac -=1};  this.enemyListService.updateTarget(this.selectedEnemy);};
   moreac = function () {if (this.selectedEnemy.ac <30) {this.selectedEnemy.ac +=1}; this.enemyListService.updateTarget(this.selectedEnemy);};
+
+  enemyList = [monsters.orc, monsters.gnoll, monsters.centaur, monsters.orc_warrior]
+
+  targetEnemy = function (enemy) {
+    this.selectedEnemy = enemy
+    this.enemyListService.updateTarget(enemy)
+  }
 
   
   constructor(
---
+++ src/app/monsters.ts
@@ -0,0 +1,8 @@
+
+export const monsters = {
+    orc      : {name:"orc", ac:1, display: "orc"  },
+    gnoll    : {name:"gnoll", ac:2,  display: "gnoll"},
+    "orc_warrior"    : {name:"orc_warrior", display: "orc warrior",  ac:9, ev:10,  hd:4 , hp:"18-38"  , mr:20, speed:10, xp:39  , attack:5  },
+    "orc_wizard"     : {name:"orc_wizard",  display: "orc wizard",  ac:2, ev:12, hd:3 , hp:"9-20"    , mr:20, speed:10, xp:132 , attack:20 },
+    "centaur"        : {name:"centaur",     display : "centaur ", ac:3, ev:7,  hd:4 , hp:"14-30"  , mr:20, speed:15, xp:112  , attack:10  },
+}
