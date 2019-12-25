3f46379487533fc99b0f75f1ad9ed64b8e7c7049
Luis Morao <luis.n.morao@gmail.com>
2019-12-25 06:53:52 -0800

    tooltip for monsters on the way

--- src/app/modules/attack-damage/components/enemy-list/enemy-list.component.ts
+++ src/app/modules/attack-damage/components/enemy-list/enemy-list.component.ts
@@ -16,6 +16,29 @@ export class EnemyListComponent implements OnInit {
   moreac = function () {if (this.selectedEnemy.ac <30) {this.selectedEnemy.ac +=1}; this.enemyListService.updateTarget(this.selectedEnemy);};
 
   enemyList = [monsters.orc, monsters.gnoll, monsters.centaur, monsters.orc_warrior]
+  toPlus = function (n: number) {
+    var res = ""
+    if (n==0) {res = ""}
+    if (0 < +n && n <= 5) {res = "+"}
+    if (5 < +n && n <=10) {res = "++"}
+    if (10<n && n<=15) {res = "+++"}
+    if (15<n && n<=20) {res = "++++"}
+    if (20< n && n <=25) {res = "+++++"}
+    if (25<n && n<=30) {res = "++++++"}
+    return res
+  }
+  toPlusMr = function (n: number) {
+    var res = ""
+    if (n==0) {res = ""}
+    if (0 < +n && n <= 20) {res = "+"}
+    if (10<n && n<=15) {res = "+++"}
+    if (15<n && n<=20) {res = "++++"}
+    if (20< n && n <=25) {res = "+++++"}
+    if (25<n && n<=30) {res = "++++++"}
+    return res
+  }
+
+
 
   targetEnemy = function (enemy) {
     this.selectedEnemy = enemy
--- src/app/monsters.ts
+++ src/app/monsters.ts
@@ -5,4 +5,5 @@ export const monsters = {
     "orc_warrior"    : {name:"orc_warrior", display: "orc warrior",  ac:9, ev:10,  hd:4 , hp:"18-38"  , mr:20, speed:10, xp:39  , attack:5  },
     "orc_wizard"     : {name:"orc_wizard",  display: "orc wizard",  ac:2, ev:12, hd:3 , hp:"9-20"    , mr:20, speed:10, xp:132 , attack:20 },
     "centaur"        : {name:"centaur",     display : "centaur ", ac:3, ev:7,  hd:4 , hp:"14-30"  , mr:20, speed:15, xp:112  , attack:10  },
+    "crimson_imp"        : {name:"crimson_imp",     display : "crimson imp ", ac:3, ev:14,  hd:4 , hp:"8-18"  , mr:40, speed:10, xp:48  , attack:10,   resistances: ["rF+++","rPois","rRot","rN+++","rTorm"], vulnerabilities: ["Cold","Holy"]},
 }
