6d644a3fa0dce91c130fc5421b13e0093ffb83c9
Luis Morao <luis.n.morao@gmail.com>
2020-01-07 06:48:22 -0800

    spell list fail chance formula

--- src/app/modules/spell-damage/spell-list/spell-list.component.ts
+++ src/app/modules/spell-damage/spell-list/spell-list.component.ts
@@ -12,13 +12,14 @@ import { Character } from '../../../shared/models/character.model'
 export class SpellListComponent implements OnInit {
   skills = new Skills()
   profile = new Character()
+  armour_penalty = 0
   diceAvg = function (number) {
     return (number -1) /2
   }
   diceMax = function (number) {
     return number 
   }
-  power = function (spell, enhancers = 0, brilliance = 0, wildMagic=0, subDueMagic = 0) {
+  power = function (spell, limited = true, enhancers = 0, brilliance = 0, wildMagic=0, subDueMagic = 0) {
     //brilliance is either 0 or 3
     var mut = 1
     var bMut = 1
@@ -30,17 +31,52 @@ export class SpellListComponent implements OnInit {
     }
     if (enhancers >3) {enhancers = 3}
     var enh = Math.pow(1.5, enhancers)
-    switch(wildMagic) {case 0: mut =  1;case 1: mut = 1.3; case 2: mut = 1.6; case 3: mut= 1.9}
+    switch (true) {
+      case (wildMagic == 0): mut =  1; break; case (wildMagic == 1): mut = 1.3; break; case (wildMagic == 2): mut = 1.6; break; case (wildMagic == 3): mut= 1.9; break;
+    }
     var raw_power = ((spellcasting/2) + 2* schools + brilliance) * enh * (this.profile.int/10) * mut * bMut * ds
     //step down could be another function
-
     var res = 50 * Math.log2(1 + raw_power/50)
+    if (limited == true) {
+      if (res > spell.max_power) {res = spell.max_power}
+    }
     return res
+  }
+  fail = function (spell) {
+    var pow = this.power(spell, false)
+    var chance = 60
+    chance -= pow
+    chance -= this.profile.int * 2
+    chance += this.armour_penalty
+
+    var chance2 
+
+    chance2 = (((chance + 426) * chance + 82670) * chance + 7245398) / 262144;
+
+//chance2 += get_form()->spellcasting_penalty;
+
+//chance2 -= 2 * you.get_mutation_level(MUT_SUBDUED_MAGIC);
+//chance2 += 4 * you.get_mutation_level(MUT_WILD_MAGIC);
+//chance2 += 4 * you.get_mutation_level(MUT_ANTI_WIZARDRY);
+
+//if (you.props.exists(SAP_MAGIC_KEY))
+//chance2 += you.props[SAP_MAGIC_KEY].get_int() * 12;
+
+//chance2 += you.duration[DUR_VERTIGO] ? 7 : 0;
+
+// Apply the effects of Vehumet and items of wizardry.
+//chance2 = _apply_spellcasting_success_boosts(spell, chance2);
+
+    if (chance2 <0) {chance2 =0 }
+    if (chance2 >100) {chance2 =100 }
+
+    return chance2
   }
   spellList = [
     {display:"Foxfire", image: "fire/foxfire.png", type1:"conjurations" , type2:"fire magic", max_power: 40,
 
       "formula" : function (power, dice, max_power = 40) { if (power > max_power) {power = max_power}; return dice(3+power/3)},
+      level: 1,
   },
     {
       display:"Freeze", 
@@ -50,7 +86,8 @@ export class SpellListComponent implements OnInit {
       max_power: 25, 
       "formula" : function (power, dice, max_power = 25) { if (power > max_power) {power = max_power}; return dice(3+power/3)},
       damage_type: "cold",
-      range: 1
+      range: 1,
+      level: 1,
     }, 
   ]
   targetSpell = function () {}
